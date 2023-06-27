d863ed637360c44a56ad9514e8eab4824d96aee1
negru-ioan <ioannegru808@gmail.com>
2023-06-27 21:37:46 +0300

    Before adding moble navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,22 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'apple': {
+          '50': '#f2fcf1',
+          '100': '#defade',
+          '200': '#c0f3bf',
+          '300': '#8de88d',
+          '400': '#54d454',
+          '500': '#32cd32',
+          '600': '#209920',
+          '700': '#1c791d',
+          '800': '#1b601c',
+          '900': '#184f1a',
+          '950': '#082b09',
+      },
+      
+      }
     },
   },
   plugins: [],
